0cf3e85191f1e58514e515a903b8d9b728b40744
rohansso <robin.hansson@capgemini.com>
2022-06-27 13:09:21 +0200

    Changed PrintOut in Cloned project

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! Change 4");
+        System.out.println("Hello World! Change in Cloned Project");
     }
 }
